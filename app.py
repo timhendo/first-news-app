42298d5b1f5f9736fbf868169668c64d2bb5f37b
timhendo <admin@admins-Mac-Pro-3.local>
2017-03-04 16:18:51 -0500

    added

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask 
 from flask import render_template
+from flask import abort
 import csv
 
 def get_csv():
@@ -13,11 +14,21 @@ def get_csv():
 app = Flask(__name__)
 
 @app.route('/')
-
 def index():
 	template="index.html"
 	object_list=get_csv()
 	return render_template(template,object_list=object_list)
+
+@app.route('/<row_id>/')
+def detail(row_id):
+	template="detail.html"
+	object_list = get_csv()
+	for row in object_list:
+		if row['id']==row_id: 	
+			return render_template(template, object=row)
+	abort(404)
+
+
 if __name__ == "__main__":
 	app.run(debug=True,use_reloader=True)
 
